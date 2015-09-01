a4a81e87a15f9532e8fba6b7e3e3fa98f918cdcb
Yoel Monzon <yoelfme@hotmail.com>
2015-09-01 17:44:50 -0600

    [add] redis component

--- todo_app/views.py
+++ todo_app/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.views.generic import View
 from .models import Task
+from redis import Redis
+
+redis = Redis(host='redis', port=6379)
 
 
 class HomeView(View):
 
     def get(self, request):
         tasks = Task.objects.all()
-        counter = 0
+        counter = redis.incr('counter')
         return render(request, 'todo_app/home.html', {
             'tasks': tasks,
             'counter': counter
